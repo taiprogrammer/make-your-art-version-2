8046d1e251e9327314c086ad7a11edc80b94d77a
taiprogrammer <taiza.marques@sptech.school>
2022-08-08 17:44:49 -0300

    feature (sign up) - finishing sections from sign up page

--- view/js/sign-up/signUpForm.js
+++ view/js/sign-up/signUpForm.js
@@ -1,12 +1,23 @@
-var personalInformation = document.getElementById('personal-information');
-var addressInformation = document.getElementById('address');
+var typeProfile = document.getElementById("profile-type");
+var addressInformation = document.getElementById("address");
+var personalInformation = document.getElementById("personal-information");
 
 function openAddressSection() {
-    personalInformation.style.display = 'none';
-    addressInformation.style.display = 'block';
+  personalInformation.style.display = "none";
+  addressInformation.style.display = "block";
 }
 
 function backToPersonalInformation() {
-    addressInformation.style.display = 'none';
-    personalInformation.style.display = 'block';
+  addressInformation.style.display = "none";
+  personalInformation.style.display = "block";
+}
+
+function openTypeProfile() {
+  addressInformation.style.display = "none";
+  typeProfile.style.display = "block";
+}
+
+function backToAddressSection() {
+  typeProfile.style.display = "none";
+  addressInformation.style.display = "block";
 }
