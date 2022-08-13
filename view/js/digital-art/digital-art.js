5a4837f3ad034e57a1478cec88440139fae1ee03
taiprogrammer <taiza.marques@sptech.school>
2022-08-13 18:25:20 -0300

    feature (digital art)

---
+++ view/js/digital-art/digital-art.js
@@ -0,0 +1,33 @@
+let isEmpty = false;
+
+function openModal(n) {
+  let modal = document.getElementById("modal");
+
+  let modalImage = document.getElementById("modalImage");
+
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+
+  modal.style.display = "block";
+
+  modalImage.src = `../assets/img/digital-art/digital-art-${n}.jpg`;
+}
+
+function closeModal() {
+  let modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
+
+function like() {
+  const iconLike = document.getElementById("like");
+
+  if (!isEmpty) {
+    isEmpty = true;
+    iconLike.src = "../assets/icons/ic-heart-filled.svg";
+  } else if (isEmpty) {
+    isEmpty = false;
+    iconLike.src = "../assets/icons/ic-heart-empty.svg";
+  }
+}
